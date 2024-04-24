734e1869aff12b08270706d214d83c6581ab4c74
Your Name <you@example.com>
2024-04-25 01:33:46 +0700

    25/4/2024

--- login.php
+++ login.php
@@ -1,3 +1,54 @@
+<?php
+
+session_start();
+
+include('server/connection.php');
+
+if(isset($_SESSION['logged_in'])){
+  header('Location: account.php');
+  exit;
+}
+
+
+if(isset($_POST['login_btn'])) {
+
+  $email = $_POST['email'];
+  $password = md5($_POST['password']);
+
+  $stmt = $conn->prepare("SELECT user_id, user_name, user_email, user_password FROM users WHERE user_email=? AND user_password=? LIMIT 1");
+
+  $stmt->bind_param('ss', $email, $password);
+
+  if($stmt->execute()) {
+    $stmt->bind_result($user_id, $user_name, $user_email, $user_password);
+    $stmt->store_result();
+
+    if($stmt->num_rows() == 1) {
+
+      $stmt->fetch();
+
+      $_SESSION['user_id'] = $user_id;
+      $_SESSION['user_name'] = $user_name;
+      $_SESSION['user_email'] = $user_email;
+      $_SESSION['logged_in'] = true;
+
+      header('location: account.php?message=logged in successfully');
+
+    } else {
+
+      header('location: login.php?error=could not verify your account');
+
+    }
+  } else {
+
+    header('location: login.php?error=something went wrong');
+
+  }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,33 +115,26 @@
         </div>
 
         <div class="mx-auto container">
-            <form action="" id="login-form">
+            <form action="login.php" id="login-form" method="POST">
+                <p style="color: red" class="text-center"> <?php if(isset($_GET['error'])){ echo $_GET['error']; } ?> </p>
                 <div class="form-group">
                     <label for="" >Email</label>
                     <input type="text" class="form-control" id="login-email" name="email" placeholder="Email" required>
                 </div>
                 <div class="form-group">
                     <label for="" >Password</label>
-                    <input type="text" class="form-control" id="login-password" name="password" placeholder="Password" required>
+                    <input type="password" class="form-control" id="login-password" name="password" placeholder="Password" required>
                 </div>
                 <div class="form-group">
-                    <input type="submit" class="btn" id="login-btn" value="Login">
+                    <input type="submit" class="btn" id="login-btn" name="login_btn" value="Login">
                 </div>
                 <div class="form-group">
-                    <a href="#" id="register-url" class="btn"> Don't have account? Register</a>
+                    <a href="register.php" id="register-url" class="btn"> Don't have account? Register</a>
                 </div>
             </form>
         </div>
 
     </section>
-
-
-
-
-
-
-
-
 
 
 
--- register.php
+++ register.php
@@ -1,5 +1,8 @@
 <?php
 
+session_start();
+
+include('server/connection.php');
 
 if(isset($_POST['register'])){
 
@@ -8,20 +11,60 @@ if(isset($_POST['register'])){
   $password = $_POST['password'];
   $confirmPassword = $_POST['confirmPassword'];
 
-}else {
+  //Kiểm tra mật khẩu xem có khớp hay không
+  if($password !== $confirmPassword){
+    header('location: register.php?error=passwords do not match');
+  }
+
+  else if(strlen($password) < 6){
+    header('location: register.php?error=password must be at least 6 characters');
+  } 
+  else {
+
+      //Kiểm tra email này có thực hay không 
+      $stmt1 = $conn->prepare("SELECT count(*) FROM users where user_email=?");
+      $stmt1->bind_param('s', $email);
+      $stmt1->execute();
+      $stmt1->bind_result($num_rows);
+      $stmt1->store_result();
+      $stmt1->fetch();
 
 
+      // Nếu Email đã được đăng ký   
+      if($num_rows != 0){
+        header('location: register.php?error=user with this email already exist');
+
+      //Nếu không có người nào đăng ký tài khoản với email trước đây
+      }else{
+
+          //Tạo mới tài khoản người dùng
+          $stmt = $conn->prepare("INSERT INTO users (user_name,user_email,user_password) 
+                          VALUE (?,?,?) ");
+
+          $stmt->bind_param('sss',$name,$email,md5($password));
+
+
+          if($stmt->execute()){
+
+              $_SESSION['user_email'] = $email;
+              $_SESSION['user_name'] = $name;
+              $_SESSION['logged_in'] = true;
+              header('location: account.php?register=Registered Successfully');
+
+          }else{
+
+            header('location: register.php?error=Could not create an account at the moment');
+
+          }
+      }
+  }
+
+}else if(isset($_SESSION['logged_in'])){
+
+  header('location: account.php');
+  exit;
 
 }
-
-
-
-
-
-
-
-
-
 
 
 
@@ -97,6 +140,7 @@ if(isset($_POST['register'])){
 
         <div class="mx-auto container">
             <form action="" id="register-form" method="POST" action="register.php">
+              <p style="color: red"> <?php if(isset($_GET['error'])){ echo $_GET['error']; }?> </p>
                 <div class="form-group">
                     <label for="" >Name</label>
                     <input type="text" class="form-control" id="register-name" name="name" placeholder="Name" required>
@@ -107,17 +151,17 @@ if(isset($_POST['register'])){
                 </div>
                 <div class="form-group">
                     <label for="" >Password</label>
-                    <input type="text" class="form-control" id="register-password" name="password" placeholder="Password" required>
+                    <input type="password" class="form-control" id="register-password" name="password" placeholder="Password" required>
                 </div>
                 <div class="form-group">
                     <label for="" >Confirm Password</label>
-                    <input type="text" class="form-control" id="register-confirmpassword" name="confirmPassword" placeholder="Confirm Password" required>
+                    <input type="password" class="form-control" id="register-confirmpassword" name="confirmPassword" placeholder="Confirm Password" required>
                 </div>
                 <div class="form-group">
                     <input type="submit" class="btn" id="register-btn" name="register" value="Register">
                 </div>
                 <div class="form-group">
-                    <a href="#" id="login-url" class="btn"> Do you have an account? Login</a>
+                    <a href="login.php" id="login-url" class="btn"> Do you have an account? Login</a>
                 </div>
             </form>
         </div>
